ee45872565fd0c02ba7f311c733acb7116882c07
Braiek01 <mohamedamine.braiek@polytechnicien.tn>
2025-11-20 22:36:11 +0100

    MORE FIXING PLEASE WORK

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig ,  importProvidersFrom } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
+import { provideRouter } from '@angular/router';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { routes } from './app.routes';
 import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    importProvidersFrom(BrowserAnimationsModule),
+    provideRouter(routes),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
