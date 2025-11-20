1dd7f9e9b8e421340b97d00e492a2667270048a0
Braiek01 <mohamedamine.braiek@polytechnicien.tn>
2025-11-20 22:21:18 +0100

    fxing routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SquadBuilderComponent } from './components/squad-builder/squad-builder.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/squad-builder', pathMatch: 'full' },
+  { path: 'squad-builder', component: SquadBuilderComponent },
+  { path: '**', redirectTo: '/squad-builder' } // Wildcard route for 404 page
+];
